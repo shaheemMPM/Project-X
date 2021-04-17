e6e99a27e15a30f6e4c588d745bcba7c5fedeb39
shaheemMPM <shaheem.mpm@gmail.com>
2021-04-17 18:24:16 +0530

    Add Landing Page Template

--- project-frontend/src/App.js
+++ project-frontend/src/App.js
@@ -1,14 +1,46 @@
+// Core Modules
 import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
+// Dependancy Modules
+import MoonLoader from 'react-spinners/MoonLoader';
+// Pages
+import Landing from './pages/Landing';
 
 class App extends Component {
   
-  state = {  }
+  state = { 
+    isLoggedIn: false,
+    isLoading: false
+  }
   
   render() { 
+    let routes;
+    if (this.state.isLoggedIn) {
+      routes =  (
+        <Switch>
+          <Route path="/" exact component={Landing} />
+        </Switch>
+      );
+    } else {
+      routes =  (
+        <Switch>
+          <Route path="/" exact component={Landing} />
+        </Switch>
+      );
+    }
     return (    
-      <div className="App">
-      
-      </div>
+      <>
+      {this.state.isLoading ? 
+				<MoonLoader
+					css={{ display: "block", margin: "25vh auto", borderColor: "red" }}
+					size={150}
+					color={"#FF0000"}
+					loading={true}
+				/>
+			: 
+				routes
+			}
+      </>
     );
   }
 }
