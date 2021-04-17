2368fc1b119e369bf7fec0e71845733189b511c7
shaheemMPM <shaheem.mpm@gmail.com>
2021-04-17 16:31:11 +0530

    Setup Project

---
+++ project-frontend/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  
+  state = {  }
+  
+  render() { 
+    return (    
+      <div className="App">
+      
+      </div>
+    );
+  }
+}
+ 
+export default App;
