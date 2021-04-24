8164e4b708f8e8f3dd57408d39374ad0b815fad0
shaheemMPM <shaheem.mpm@gmail.com>
2021-04-25 04:17:59 +0530

    setup signup

--- project-frontend/src/App.js
+++ project-frontend/src/App.js
@@ -1,5 +1,5 @@
 // Core Modules
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Redirect, Route, Switch } from 'react-router-dom';
 // Dependancy Modules
 import MoonLoader from 'react-spinners/MoonLoader';
@@ -18,54 +18,72 @@ import PageNotFound from './pages/404';
 
 const App = () => {
 
-  const [isLoggedIn, setIsLoggedIn] = useState(true);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [isLoading, setIsLoading] = useState(true);
   
-  
-    let routes;
-    if (isLoggedIn) {
-      routes =  (
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Redirect path="/login" exact to="/" />
-          <Redirect path="/signin" exact to="/" />
-          <Redirect path="/signup" exact to="/" />
-          <Redirect path="/register" exact to="/" />
-          <Route path="/class/:id" exact component={Classroom} />
-          <Route path="/class/:id/upload-lecture" exact component={UploadLecture} />
-          <Route path="/class/:id/upload-material" exact component={UploadMaterial} />
-          <Route path="/class/:id/upload-assignment" exact component={UploadAssignment} />
-          <Route path="/class/:id/lecture/:lid" exact component={Lecture} />
-          <Route path="/create-classroom" exact component={CreateClassroom} />
-          <Route path="*" component={PageNotFound} />
-        </Switch>
-      );
-    } else {
-      routes =  (
-        <Switch>
-          <Route path="/" exact component={Landing} />
-          <Route path="/signin" exact component={SignIn} />
-          <Redirect path="/login" exact to ="/signin" />
-          <Route path="/signup" exact component={SignUp} />
-          <Redirect path="/register" exact to ="/signup" />
-          <Route path="*" component={PageNotFound} />
-        </Switch>
-      );
-    }
-    return (    
-      <>
-      {isLoading ? 
-				<MoonLoader
-					css={{ display: "block", margin: "25vh auto", borderColor: "red" }}
-					size={150}
-					color={"#FF0000"}
-					loading={true}
-				/>
-			: 
-				routes
-			}
-      </>
+  useEffect(() => {
+    setIsLoading(true);
+    let cachedData = sessionStorage.getItem("auth_data");
+		if (!!cachedData && cachedData !== "null") {
+      setIsLoading(false);
+      setIsLoggedIn(true);
+		} else {
+      setIsLoading(false);
+		}
+  }, []);
+
+	const AuthChange = (data) => {
+    setIsLoggedIn(true);
+    sessionStorage.setItem("auth_data", JSON.stringify(data));
+    window.location.pathname = "/";
+	};
+
+  let routes;
+  if (isLoggedIn) {
+    routes =  (
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Redirect path="/login" exact to="/" />
+        <Redirect path="/signin" exact to="/" />
+        <Redirect path="/signup" exact to="/" />
+        <Redirect path="/register" exact to="/" />
+        <Route path="/class/:id" exact component={Classroom} />
+        <Route path="/class/:id/upload-lecture" exact component={UploadLecture} />
+        <Route path="/class/:id/upload-material" exact component={UploadMaterial} />
+        <Route path="/class/:id/upload-assignment" exact component={UploadAssignment} />
+        <Route path="/class/:id/lecture/:lid" exact component={Lecture} />
+        <Route path="/create-classroom" exact component={CreateClassroom} />
+        <Route path="*" component={PageNotFound} />
+      </Switch>
     );
+  } else {
+    routes =  (
+      <Switch>
+        <Route path="/" exact component={Landing} />
+        <Route path="/signin" exact component={SignIn} />
+        <Redirect path="/login" exact to ="/signin" />
+        <Route path="/signup" exact render={(props) => (
+            <SignUp {...props} logHandler={AuthChange} />
+          )} />
+        <Redirect path="/register" exact to ="/signup" />
+        <Route path="*" component={PageNotFound} />
+      </Switch>
+    );
+  }
+  return (    
+    <>
+    {isLoading ? 
+      <MoonLoader
+        css={{ display: "block", margin: "25vh auto", borderColor: "red" }}
+        size={150}
+        color={"#FF0000"}
+        loading={true}
+      />
+    : 
+      routes
+    }
+    </>
+  );
 }
  
 export default React.memo(App);
--- project-frontend/src/pages/SignUp.jsx
+++ project-frontend/src/pages/SignUp.jsx
@@ -1,3 +1,5 @@
+// Core Componets
+import { useState } from 'react';
 // Styles
 import '../public/SignIn/vendor/bootstrap/css/bootstrap.min.css';
 import '../public/SignIn/fonts/font-awesome-4.7.0/css/font-awesome.min.css';
@@ -12,83 +14,149 @@ import '../public/SignIn/css/util.css';
 import '../public/SignIn/css/main.css';
 // Images
 import BgImage from '../public/SignIn/images/bg-01.jpg';
+// Dependancy Modules
+import MoonLoader from "react-spinners/MoonLoader";
+import swal from 'sweetalert';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
 
-const SingUp = () => {
-    return (
-        <div className="limiter">
-            <div className="container-login100">
+const SingUp = (props) => {
 
-                <div className="login100-more" style={{backgroundImage: `url(${BgImage})`}}></div>
+    const [isLoading, setIsLoading] = useState(false);
+    const [name, setName] = useState('');
+    const [userName, setUserName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [repeatPassword, setRepeatPassword] = useState('');
+    const [isTermsRead, setIsTermsRead] = useState(false);
 
-                <div className="wrap-login100 p-l-50 p-r-50 p-t-72 p-b-50">
-                    <form className="login100-form validate-form">
-                        <span className="login100-form-title p-b-59">
-                            Sign Up
-                        </span>
+    const SignupHandler = () => {
+        if (!name || !userName || !email || !password || !repeatPassword) {
+            swal("", "Fill all the field", "info");
+            return;
+        }
+        let re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        if (!re.test(email) ) {
+            swal("", "Enter Valid Email Id", "warning");
+            return;
+        }
+        if (password !== repeatPassword) {
+            swal("", "passwords should be same", "warning");
+            return;
+        }
+        if (password.length < 6) {
+            swal("", "Password should be minimum 6 character long", "warning");
+            return;
+        }
+        if (!isTermsRead) {
+            swal("", "Agree to the terms of user", "error");
+            return;
+        }
 
-                        <div className="wrap-input100 validate-input" data-validate="Name is required">
-                            <span className="label-input100">Full Name</span>
-                            <input className="input100" type="text" name="name" placeholder="Name..." />
-                            <span className="focus-input100"></span>
-                        </div>
+        setIsLoading(true);
+        const POST_URL = 'http://localhost:8000/api/v1/user/signup';
+        axios.post(POST_URL, {
+            "name": name,
+            "username": userName,
+            "email": email,
+            "password": password
+        }).then((response) => {
+            props.logHandler(response.data.token);    
+        }).catch((error) => {
+            swal("Error", error.response.data.message, "error").then(() => {
+                setIsLoading(false);
+            });
+        });
+    }
 
-                        <div className="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
-                            <span className="label-input100">Email</span>
-                            <input className="input100" type="text" name="email" placeholder="Email addess..." />
-                            <span className="focus-input100"></span>
-                        </div>
+    let content;
+    if (isLoading) {
+        content = (
+            <MoonLoader
+                css={{display: 'block', margin: '25vh auto',borderColor: 'red'}}
+                size={150}
+                color={"#FF0000"}
+                loading={true} />
+        );
+    } else {
+        content = (
+            <div className="limiter">
+                <div className="container-login100">
 
-                        <div className="wrap-input100 validate-input" data-validate="Username is required">
-                            <span className="label-input100">Username</span>
-                            <input className="input100" type="text" name="username" placeholder="Username..." />
-                            <span className="focus-input100"></span>
-                        </div>
+                    <div className="login100-more" style={{backgroundImage: `url(${BgImage})`}}></div>
 
-                        <div className="wrap-input100 validate-input" data-validate = "Password is required">
-                            <span className="label-input100">Password</span>
-                            <input className="input100" type="text" name="pass" placeholder="*************" />
-                            <span className="focus-input100"></span>
-                        </div>
+                    <div className="wrap-login100 p-l-50 p-r-50 p-t-72 p-b-50">
+                        <div className="login100-form validate-form">
+                            <span className="login100-form-title p-b-59">
+                                Sign Up
+                            </span>
 
-                        <div className="wrap-input100 validate-input" data-validate = "Repeat Password is required">
-                            <span className="label-input100">Repeat Password</span>
-                            <input className="input100" type="text" name="repeat-pass" placeholder="*************" />
-                            <span className="focus-input100"></span>
-                        </div>
-
-                        <div className="flex-m w-full p-b-33">
-                            <div className="contact100-form-checkbox">
-                                <input className="input-checkbox100" id="ckb1" type="checkbox" name="remember-me" />
-                                <label className="label-checkbox100" htmlFor="ckb1">
-                                    <span className="txt1">
-                                        I agree to the
-                                        <a href="/#" className="txt2 hov1">
-                                            Terms of User
-                                        </a>
-                                    </span>
-                                </label>
-                            </div> 
-                        </div>
-
-                        <div className="container-login100-form-btn">
-                            <div className="wrap-login100-form-btn">
-                                <div className="login100-form-bgbtn"></div>
-                                <button className="login100-form-btn signin-button">
-                                    Sign Up
-                                </button>
+                            <div className="wrap-input100 validate-input" data-validate="Name is required">
+                                <span className="label-input100">Full Name</span>
+                                <input className="input100" type="text" name="name" placeholder="Name..." onChange={(e) => {setName(e.target.value)}} />
+                                <span className="focus-input100"></span>
                             </div>
 
-                            <a href="/#" className="dis-block txt3 hov1 p-r-30 p-t-10 p-b-10 p-l-30">
-                                Sign in
-                                <i className="fa fa-long-arrow-right m-l-5"></i>
-                            </a>
+                            <div className="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
+                                <span className="label-input100">Email</span>
+                                <input className="input100" type="text" name="email" placeholder="Email addess..." onChange={(e) => {setEmail(e.target.value)}} />
+                                <span className="focus-input100"></span>
+                            </div>
+
+                            <div className="wrap-input100 validate-input" data-validate="Username is required">
+                                <span className="label-input100">Username</span>
+                                <input className="input100" type="text" name="username" placeholder="Username..." onChange={(e) => {setUserName(e.target.value)}} />
+                                <span className="focus-input100"></span>
+                            </div>
+
+                            <div className="wrap-input100 validate-input" data-validate = "Password is required">
+                                <span className="label-input100">Password</span>
+                                <input className="input100" type="password" name="pass" placeholder="*************" onChange={(e) => {setPassword(e.target.value)}} />
+                                <span className="focus-input100"></span>
+                            </div>
+
+                            <div className="wrap-input100 validate-input" data-validate = "Repeat Password is required">
+                                <span className="label-input100">Repeat Password</span>
+                                <input className="input100" type="password" name="repeat-pass" placeholder="*************" onChange={(e) => {setRepeatPassword(e.target.value)}} />
+                                <span className="focus-input100"></span>
+                            </div>
+
+                            <div className="flex-m w-full p-b-33">
+                                <div className="contact100-form-checkbox">
+                                    <input className="input-checkbox100" id="ckb1" type="checkbox" name="terms" onChange={(e) => {setIsTermsRead(e.target.checked)}} />
+                                    <label className="label-checkbox100" htmlFor="ckb1">
+                                        <span className="txt1">
+                                            I agree to the
+                                            <a href="/#" className="txt2 hov1">
+                                                Terms of User
+                                            </a>
+                                        </span>
+                                    </label>
+                                </div> 
+                            </div>
+
+                            <div className="container-login100-form-btn">
+                                <div className="wrap-login100-form-btn">
+                                    <div className="login100-form-bgbtn"></div>
+                                    <button className="login100-form-btn signin-button" onClick={SignupHandler}>
+                                        Sign Up
+                                    </button>
+                                </div>
+
+                                <Link to="/signin" className="dis-block txt3 hov1 p-r-30 p-t-10 p-b-10 p-l-30">
+                                    Sign in
+                                    <i className="fa fa-long-arrow-right m-l-5"></i>
+                                </Link>
+                            </div>
                         </div>
-                    </form>
+                    </div>
+                    
                 </div>
-                
             </div>
-        </div>
-    );
+        );
+    }
+
+    return content;
 }
 
 export default SingUp;
