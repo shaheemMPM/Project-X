c0cf02636bade6dd91b397c95c89f5df25286d70
shaheemMPM <shaheem.mpm@gmail.com>
2021-05-01 07:23:15 +0530

    Adds SignIn Integration to frontend

--- project-frontend/src/App.js
+++ project-frontend/src/App.js
@@ -1,46 +1,45 @@
 // Core Modules
-import React, { useEffect, useState } from 'react';
-import { Redirect, Route, Switch } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { Redirect, Route, Switch } from "react-router-dom";
 // Dependancy Modules
-import MoonLoader from 'react-spinners/MoonLoader';
+import MoonLoader from "react-spinners/MoonLoader";
 // Pages
-import Landing from './pages/Landing';
-import SignIn from './pages/SignIn';
-import SignUp from './pages/SignUp';
-import Home from './pages/Home';
-import CreateClassroom from './pages/CreateClassroom';
-import Classroom from './pages/Classroom';
-import UploadLecture from './pages/UploadLecture';
-import UploadMaterial from './pages/UploadMaterial';
-import UploadAssignment from './pages/UploadAssignment';
-import Lecture from './pages/Lecture';
-import PageNotFound from './pages/404';
+import Landing from "./pages/Landing";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Home from "./pages/Home";
+import CreateClassroom from "./pages/CreateClassroom";
+import Classroom from "./pages/Classroom";
+import UploadLecture from "./pages/UploadLecture";
+import UploadMaterial from "./pages/UploadMaterial";
+import UploadAssignment from "./pages/UploadAssignment";
+import Lecture from "./pages/Lecture";
+import PageNotFound from "./pages/404";
 
-const App = () => {
-
+const App = (props) => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [isLoading, setIsLoading] = useState(true);
-  
+
   useEffect(() => {
     setIsLoading(true);
     let cachedData = sessionStorage.getItem("auth_data");
-		if (!!cachedData && cachedData !== "null") {
+    if (!!cachedData && cachedData !== "null") {
       setIsLoading(false);
       setIsLoggedIn(true);
-		} else {
+    } else {
       setIsLoading(false);
-		}
+    }
   }, []);
 
-	const AuthChange = (data) => {
+  const AuthChange = (data) => {
     setIsLoggedIn(true);
     sessionStorage.setItem("auth_data", JSON.stringify(data));
     window.location.pathname = "/";
-	};
+  };
 
   let routes;
   if (isLoggedIn) {
-    routes =  (
+    routes = (
       <Switch>
         <Route path="/" exact component={Home} />
         <Redirect path="/login" exact to="/" />
@@ -48,42 +47,60 @@ const App = () => {
         <Redirect path="/signup" exact to="/" />
         <Redirect path="/register" exact to="/" />
         <Route path="/class/:id" exact component={Classroom} />
-        <Route path="/class/:id/upload-lecture" exact component={UploadLecture} />
-        <Route path="/class/:id/upload-material" exact component={UploadMaterial} />
-        <Route path="/class/:id/upload-assignment" exact component={UploadAssignment} />
+        <Route
+          path="/class/:id/upload-lecture"
+          exact
+          component={UploadLecture}
+        />
+        <Route
+          path="/class/:id/upload-material"
+          exact
+          component={UploadMaterial}
+        />
+        <Route
+          path="/class/:id/upload-assignment"
+          exact
+          component={UploadAssignment}
+        />
         <Route path="/class/:id/lecture/:lid" exact component={Lecture} />
         <Route path="/create-classroom" exact component={CreateClassroom} />
         <Route path="*" component={PageNotFound} />
       </Switch>
     );
   } else {
-    routes =  (
+    routes = (
       <Switch>
         <Route path="/" exact component={Landing} />
-        <Route path="/signin" exact component={SignIn} />
-        <Redirect path="/login" exact to ="/signin" />
-        <Route path="/signup" exact render={(props) => (
-            <SignUp {...props} logHandler={AuthChange} />
-          )} />
-        <Redirect path="/register" exact to ="/signup" />
+        <Route
+          path="/signin"
+          exact
+          render={(props) => <SignIn {...props} logHandler={AuthChange} />}
+        />
+        <Redirect path="/login" exact to="/signin" />
+        <Route
+          path="/signup"
+          exact
+          render={(props) => <SignUp {...props} logHandler={AuthChange} />}
+        />
+        <Redirect path="/register" exact to="/signup" />
         <Route path="*" component={PageNotFound} />
       </Switch>
     );
   }
-  return (    
+  return (
     <>
-    {isLoading ? 
-      <MoonLoader
-        css={{ display: "block", margin: "25vh auto", borderColor: "red" }}
-        size={150}
-        color={"#FF0000"}
-        loading={true}
-      />
-    : 
-      routes
-    }
+      {isLoading ? (
+        <MoonLoader
+          css={{ display: "block", margin: "25vh auto", borderColor: "red" }}
+          size={150}
+          color={"#FF0000"}
+          loading={true}
+        />
+      ) : (
+        routes
+      )}
     </>
   );
-}
- 
+};
+
 export default React.memo(App);
--- project-frontend/src/pages/SignIn.jsx
+++ project-frontend/src/pages/SignIn.jsx
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 // Styles
 import '../public/SignIn/vendor/bootstrap/css/bootstrap.min.css';
 import '../public/SignIn/fonts/font-awesome-4.7.0/css/font-awesome.min.css';
@@ -12,56 +13,108 @@ import '../public/SignIn/css/util.css';
 import '../public/SignIn/css/main.css';
 // Images
 import BgImage from '../public/SignIn/images/bg-02.jpg';
+// Dependancy Modules
+import MoonLoader from "react-spinners/MoonLoader";
+import swal from 'sweetalert';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
 
-const SignIn = () => {
-    return (
-        <div className="limiter">
-            <div className="container-login100">
+const SignIn = (props) => {
 
-                <div className="wrap-login100 p-l-50 p-r-50 p-t-72 p-b-50">
-                    <form className="login100-form validate-form">
-                        <span className="login100-form-title p-b-59">
-                            Sign In
-                        </span>
+    const [isLoading, setIsLoading] = useState(false);
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
 
-                        
+    const SigninHandler = () => {
+        if (!email || !password) {
+            swal("", "Fill all the field", "info");
+            return;
+        }
+        let re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        if (!re.test(email) ) {
+            swal("", "Enter Valid Email Id", "warning");
+            return;
+        }
+        if (password.length < 6) {
+            swal("", "Password should be minimum 6 character long", "warning");
+            return;
+        }
 
-                        <div className="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
-                            <span className="label-input100">Email</span>
-                            <input className="input100" type="text" name="email" placeholder="Email addess..." />
-                            <span className="focus-input100"></span>
-                        </div>
+        setIsLoading(true);
+        const POST_URL = 'http://localhost:8000/api/v1/user/signin';
+        axios.post(POST_URL, {
+            "email": email,
+            "password": password
+        }).then((response) => {
+            props.logHandler(response.data.token);    
+        }).catch((error) => {
+            swal("Error", error.response.data.message, "error").then(() => {
+                setIsLoading(false);
+            });
+        });
+    }
 
-                        
+    let content;
+    if (isLoading) {
+        content = (
+            <MoonLoader
+                css={{display: 'block', margin: '25vh auto',borderColor: 'red'}}
+                size={150}
+                color={"#FF0000"}
+                loading={true} />
+        );
+    } else {
+        content = (
+            <div className="limiter">
+                <div className="container-login100">
 
-                        <div className="wrap-input100 validate-input" data-validate = "Password is required">
-                            <span className="label-input100">Password</span>
-                            <input className="input100" type="text" name="pass" placeholder="*************" />
-                            <span className="focus-input100"></span>
-                        </div>
+                    <div className="wrap-login100 p-l-50 p-r-50 p-t-72 p-b-50">
+                        <form className="login100-form validate-form">
+                            <span className="login100-form-title p-b-59">
+                                Sign In
+                            </span>
 
-                       
+                            
 
-                        <div className="container-login100-form-btn">
-                            <a href="/#" className="dis-block txt3 hov1 p-r-30 p-t-10 p-b-10 p-l-30">
-                                <i className="fa fa-long-arrow-left m-l-5"></i> Sign Up
-                            </a>
-
-                            <div className="wrap-login100-form-btn">
-                                <div className="login100-form-bgbtn"></div>
-                                <button className="login100-form-btn signin-button">
-                                    Sign In
-                                </button>
+                            <div className="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
+                                <span className="label-input100">Email</span>
+                                <input className="input100" type="text" name="email" placeholder="Email addess..." onChange={(e) => {setEmail(e.target.value)}} />
+                                <span className="focus-input100"></span>
                             </div>
-                        </div>
-                    </form>
-                </div>
 
-                <div className="login100-more" style={{backgroundImage: `url(${BgImage})`}}></div>
-                
+                            
+
+                            <div className="wrap-input100 validate-input" data-validate = "Password is required">
+                                <span className="label-input100">Password</span>
+                                <input className="input100" type="password" name="pass" placeholder="*************" onChange={(e) => {setPassword(e.target.value)}} />
+                                <span className="focus-input100"></span>
+                            </div>
+
+                        
+
+                            <div className="container-login100-form-btn">
+                                <Link to="/signup" className="dis-block txt3 hov1 p-r-30 p-t-10 p-b-10 p-l-30">
+                                    <i className="fa fa-long-arrow-left m-l-5"></i> Sign Up
+                                </Link>
+
+                                <div className="wrap-login100-form-btn">
+                                    <div className="login100-form-bgbtn"></div>
+                                    <button className="login100-form-btn signin-button" onClick={SigninHandler}>
+                                        Sign In
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+
+                    <div className="login100-more" style={{backgroundImage: `url(${BgImage})`}}></div>
+                    
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
+
+    return content;
 }
 
 export default SignIn;
