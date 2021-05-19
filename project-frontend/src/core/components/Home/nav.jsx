1ffe38ea3ae388ccae33c66208d606eca9ff0f66
shaheemMPM <shaheem.mpm@gmail.com>
2021-05-19 22:21:19 +0530

    adds navigation to create-classrooms

--- project-frontend/src/core/components/Home/nav.jsx
+++ project-frontend/src/core/components/Home/nav.jsx
@@ -1,6 +1,8 @@
-import '../../../public/Home/css/nav.css';
+import "../../../public/Home/css/nav.css";
+import { useHistory } from "react-router-dom";
 
 const Nav = () => {
+  let history = useHistory();
   return (
     <nav className="nav" role="navigation">
       <div className="nav__left">
@@ -8,13 +10,20 @@ const Nav = () => {
       </div>
       <div className="nav__right">
         <button className="btn__join">Join</button>
-        <button className="btn__create">Create</button>
+        <button
+          onClick={() => {
+            history.push("/create-classroom");
+          }}
+          className="btn__create"
+        >
+          Create
+        </button>
         <button className="btn__user">
           <span>M</span>
         </button>
       </div>
     </nav>
   );
-}
+};
 
 export default Nav;
