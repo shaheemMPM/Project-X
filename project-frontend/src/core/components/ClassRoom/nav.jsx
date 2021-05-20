19871d5bd6df7e5e450c254d3b317b9dc8e12bae
shaheemMPM <shaheem.mpm@gmail.com>
2021-05-20 19:17:05 +0530

    Adds user data and logout to navs

--- project-frontend/src/core/components/ClassRoom/nav.jsx
+++ project-frontend/src/core/components/ClassRoom/nav.jsx
@@ -1,19 +1,61 @@
-import '../../../public/Home/css/nav.css';
+import "../../../public/Home/css/nav.css";
+import { useHistory } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 const Nav = () => {
+  let history = useHistory();
+
+  const [token, setToken] = useState(null);
+  const [isDropped, setIsDropped] = useState(false);
+
+  useEffect(() => {
+    let authData = JSON.parse(sessionStorage.getItem("auth_data"));
+    setToken(authData);
+    // eslint-disable-next-line
+  }, []);
+
+  const logout = (e) => {
+    e.preventDefault();
+    sessionStorage.setItem("auth_data", "");
+    window.location.pathname = "/";
+  };
+
   return (
     <nav className="nav" role="navigation">
       <div className="nav__left">
-        <h1>Project-X</h1>
+        <h1
+          onClick={() => {
+            history.push("/");
+          }}
+        >
+          Project-X
+        </h1>
       </div>
       <div className="nav__right">
         <button className="btn__join">Live</button>
-        <button className="btn__user">
-          <span>M</span>
-        </button>
+        <div className="dropdown">
+          <button
+            className="btn__user dropbtn"
+            onClick={() => {
+              setIsDropped(!isDropped);
+            }}
+          >
+            <span>{token?.username[0] || ".."}</span>
+          </button>
+          <div
+            className="dropdown-content"
+            style={{ display: isDropped ? "block" : "none" }}
+          >
+            <a href="/#">Hey {token?.username}</a>
+            <hr style={{ margin: "0", padding: "0" }} />
+            <a href="/#" onClick={logout}>
+              Logout
+            </a>
+          </div>
+        </div>
       </div>
     </nav>
   );
-}
+};
 
 export default Nav;
