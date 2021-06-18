f441ae73b608951f1b0e491b455855ba1524cbce
shaheemMPM <shaheem.mpm@gmail.com>
2021-06-18 21:59:20 +0530

    Adds keyphrase extraction function

---
+++ keyphase-detector/keyphrase.py
@@ -0,0 +1,17 @@
+import spacy
+from collections import Counter
+from string import punctuation
+
+nlp = spacy.load("en_core_web_sm")
+
+def get_hotwords(text):
+    result = []
+    pos_tag = ['PROPN', 'ADJ', 'NOUN'] 
+    doc = nlp(text.lower()) 
+    for token in doc:
+        if(token.text in nlp.Defaults.stop_words or token.text in punctuation):
+            continue
+        if(token.pos_ in pos_tag):
+            result.append(token.text)
+                
+    return result
