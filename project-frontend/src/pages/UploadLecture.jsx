34d8da9b447c5734f4ab8238f3b1af6fbaf945c3
shaheemMPM <shaheem.mpm@gmail.com>
2021-06-23 23:18:27 +0530

    Adds loading into upload lecture

--- project-frontend/src/pages/UploadLecture.jsx
+++ project-frontend/src/pages/UploadLecture.jsx
@@ -4,11 +4,13 @@ import { useHistory } from "react-router-dom";
 import Nav from "../core/components/UploadItem/nav";
 import swal from "sweetalert";
 import axios from "axios";
+import MoonLoader from "react-spinners/MoonLoader";
 // import style
 import "../public/UploadItem/main.css";
 
 const UploadLecture = (props) => {
   const classId = props.match.params.id;
+  const [isLoading, setIsLoading] = useState(false);
   const [token, setToken] = useState(null);
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
@@ -25,6 +27,7 @@ const UploadLecture = (props) => {
   const uploadLectureHandler = (event) => {
     event.preventDefault();
     if (title && image) {
+      setIsLoading(true);
       let formData = new FormData();
       formData.append("myfile", image);
       formData.append("title", title);
@@ -48,73 +51,87 @@ const UploadLecture = (props) => {
     }
   };
 
-  return (
-    <>
-      <Nav />
-      <div className="upload-lecture">
-        <h1 className="upload-header">UploadLecture</h1>
+  let content;
+  if (isLoading) {
+    content = (
+      <MoonLoader
+        css={{ display: "block", margin: "25vh auto", borderColor: "red" }}
+        size={150}
+        color={"#FF0000"}
+        loading={true}
+      />
+    );
+  } else {
+    content = (
+      <>
+        <Nav />
+        <div className="upload-lecture">
+          <h1 className="upload-header">UploadLecture</h1>
 
-        <form className="login100-form ">
-          <div className="wrap-input100">
-            <span className="label-input100">Title</span>
-            <input
-              className="input100"
-              type="text"
-              name="name"
-              placeholder="Title..."
-              onChange={(e) => {
-                setTitle(e.target.value);
-              }}
-            />
-            <span className="focus-input100"></span>
-          </div>
-
-          <div className="wrap-input100">
-            <span className="label-input100">Description</span>
-            <textarea
-              className="input100 desc-textarea"
-              placeholder="Description..."
-              onChange={(e) => {
-                setDescription(e.target.value);
-              }}
-            ></textarea>
-            <span className="focus-input100"></span>
-          </div>
-
-          <div className="wrap-input100 select-file">
-            <span className="label-input100 select-file-label">
-              Select File
-            </span>
-            <input
-              className="input100"
-              type="file"
-              name="repeat-pass"
-              accept=".mp4"
-              onChange={(e) => {
-                const file = e.target.files[0];
-                setImage(file);
-              }}
-            />
-            <small className="footnote">
-              Only select <span>.mp4</span> files
-            </small>
-          </div>
-
-          <div className="container-login100-form-btn">
-            <div className="wrap-login100-form-btn">
-              <div className="login100-form-bgbtn"></div>
-              <button
-                className="login100-form-btn upload-button"
-                onClick={uploadLectureHandler}
-              >
-                Upload
-              </button>
+          <form className="login100-form ">
+            <div className="wrap-input100">
+              <span className="label-input100">Title</span>
+              <input
+                className="input100"
+                type="text"
+                name="name"
+                placeholder="Title..."
+                onChange={(e) => {
+                  setTitle(e.target.value);
+                }}
+              />
+              <span className="focus-input100"></span>
             </div>
-          </div>
-        </form>
-      </div>
-    </>
-  );
+
+            <div className="wrap-input100">
+              <span className="label-input100">Description</span>
+              <textarea
+                className="input100 desc-textarea"
+                placeholder="Description..."
+                onChange={(e) => {
+                  setDescription(e.target.value);
+                }}
+              ></textarea>
+              <span className="focus-input100"></span>
+            </div>
+
+            <div className="wrap-input100 select-file">
+              <span className="label-input100 select-file-label">
+                Select File
+              </span>
+              <input
+                className="input100"
+                type="file"
+                name="repeat-pass"
+                accept=".mp4"
+                onChange={(e) => {
+                  const file = e.target.files[0];
+                  setImage(file);
+                }}
+              />
+              <small className="footnote">
+                Only select <span>.mp4</span> files
+              </small>
+            </div>
+
+            <div className="container-login100-form-btn">
+              <div className="wrap-login100-form-btn">
+                <div className="login100-form-bgbtn"></div>
+                <button
+                  className="login100-form-btn upload-button"
+                  onClick={uploadLectureHandler}
+                >
+                  Upload
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </>
+    );
+  }
+
+  return content;
 };
 
 export default UploadLecture;
