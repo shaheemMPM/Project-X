520ab0960f20b0b4c4e759302b72758aec59ad8a
shaheemMPM <shaheem.mpm@gmail.com>
2021-06-24 10:54:41 +0530

    inetegrates display assignment apis

--- project-frontend/src/pages/Classroom.jsx
+++ project-frontend/src/pages/Classroom.jsx
@@ -16,6 +16,7 @@ const Classroom = (props) => {
   const [classData, setClassData] = useState(null);
   const [lectures, setLectures] = useState(null);
   const [materials, setMaterials] = useState(null);
+  const [assignments, setAssignments] = useState(null);
 
   useEffect(() => {
     let authData = JSON.parse(sessionStorage.getItem("auth_data"));
@@ -77,11 +78,30 @@ const Classroom = (props) => {
       });
   };
 
+  const getAssignments = () => {
+    if (!token) {
+      return;
+    }
+    const GET_URL = `http://localhost:8000/api/v1/assignment/class/${classId}`;
+    const header_config = {
+      headers: { Authorization: `Bearer ${token.token}` },
+    };
+    axios
+      .get(GET_URL, header_config)
+      .then((response) => {
+        setAssignments(response.data.data);
+      })
+      .catch((error) => {
+        swal("Error", error.response.data.message, "error");
+      });
+  };
+
   useEffect(
     () => {
       getClassroomData();
       getLectures();
       getMaterials();
+      getAssignments();
     },
     // eslint-disable-next-line
     [token]
@@ -105,6 +125,7 @@ const Classroom = (props) => {
             />
             <Assignments
               isAuthor={classData.createdBy === token.username}
+              assignments={assignments}
               classId={classId}
             />
           </section>
